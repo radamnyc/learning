a86516f4308ef8dd695fe07b522ddfea63e64583
adam <adam@splickit.com>
2014-04-12 22:31:39 -0600

    initialize repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DineApp::Application.config.secret_token = '73278eb3972dc35f285cd6605fc676b932dd6f6bbe09111a4f0c50d1e6361688d504f6a48c07ecc098c4e6ba9cf491e9a6dcb061a6cef05f27f880888710be02'
